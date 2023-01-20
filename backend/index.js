384e7769d8d08fde5cb5d6fa653e8fe6efdbe642
jamilahmed2 <ja093599@gmail.com>
2023-01-21 02:02:35 +0500

    connecting to the database

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
+import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+
+
+
+mongoose.set('strictQuery', true);
+const mongoURI = "mongodb+srv://socialmediappmern:socialmediapp123@cluster0.vfpg1kg.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(mongoURI, { useNewUrlParser: true }, { useUnifiedToplogy: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
+    .catch ((error) => console.log(error.message));
