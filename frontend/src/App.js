16eb0018693e51190699f6c2e25f0c021e2f9733
jamilahmed2 <ja093599@gmail.com>
2023-01-22 01:44:45 +0500

    jsx - material ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import {Container,AppBar,Typography,Grow,Grid} from '@mui/material';
+import timeline from './Images/timeline.jpeg'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 const App = () => {
   return (
-    <>
-    </>
+    <Container maxwidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography varient="h2" aling="center">Timeline</Typography>
+        <img src={timeline} alt='timeline' height="100"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
---
+++ frontend/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from './Post/Post'
+
+const Posts = () => {
+  return (
+    <div>
+        Posts
+        <Post/>
+    </div>
+  )
+}
+
+export default Posts
