d600712d9f14db0eff4cbd503ff4697b90eedbfd
jamilahmed2 <ja0935991@gmail.com>
2023-02-07 04:11:45 +0500

    fixed

--- backend/index.js
+++ backend/index.js
@@ -19,20 +19,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 mongoose.set('strictQuery', true);
-const PORT = process.env.PORT ;
-
-app.use(express.static(path.join(__dirname, "./frontend/build/index.html")))
-
-app.get("*",function(_,res){
-    res.sendFile(
-        path.join(__dirname,'./frontend/build/index.html'),
-        function(err){
-            if(err){
-                res.status(500).send(err);
-            }
-        }
-    )
-})
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, { useUnifiedToplogy: true })
 .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
