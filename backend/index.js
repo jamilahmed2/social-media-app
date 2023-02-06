8274d434f7524fc9bacba1c9ed6b99a3c94ec56f
jamilahmed2 <ja0935991@gmail.com>
2023-02-07 01:35:49 +0500

    env variables

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import postRoutes from "./routes/Posts.js"
+import dotenv from 'dotenv';
 
-const app = express()
-
+const app = express();
+dotenv.config();
 app.use(express.json());
 app.use(bodyParser.json());
 
@@ -18,9 +19,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 mongoose.set('strictQuery', true);
-const mongoURI = "mongodb+srv://socialmediappmern:socialmediapp123@cluster0.vfpg1kg.mongodb.net/?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
-mongoose.connect(mongoURI, { useNewUrlParser: true }, { useUnifiedToplogy: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true }, { useUnifiedToplogy: true })
 .then(() => app.listen(PORT, () => console.log(`server running on ${PORT}`)))
     .catch ((error) => console.log(error.message));
