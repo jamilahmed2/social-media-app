d92101ba9a2bdfc3c349c2f8572bb3db64b68793
jamilahmed2 <ja0935991@gmail.com>
2023-03-07 02:05:06 +0500

    deploy

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-// const API = axios.create({ baseURL: 'http://localhost:5000' });
-const API = axios.create({ baseURL: 'http://localhost:5000' || 'https://timeline-pro.herokuapp.com' });
+const API = axios.create({ baseURL: 'https://timeline-pro.herokuapp.com' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' || 'https://timeline-pro.herokuapp.com' });
 
 // without interceptors middleware will not work
 API.interceptors.request.use((req) => {
